90b0939fb81c4756d3bb13733e287f1cc2a2bbe2
Luna <git@l4.pm>
2025-01-13 16:08:36 -0300

    fix typo

--- src/state/queries/messages/const.ts
+++ src/state/queries/messages/const.ts
@@ -1,4 +1,4 @@
-import {BSKY_CHAT_DID_REFERENCE} from './lib/constants'
+import {BSKY_CHAT_DID_REFERENCE} from '#/lib/constants'
 export const DM_SERVICE_HEADERS = {
   'atproto-proxy': BSKY_CHAT_DID_REFERENCE,
 }
